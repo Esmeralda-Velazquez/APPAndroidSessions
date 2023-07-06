83aff80b6701e5b2c23aaba4ec2d0abedb39eb11
Esmeralda <velazquezesmeraldard@gmail.com>
2023-07-06 07:07:21 -0600

    PANTALLA  SUMMMARY AGREGADA

--- app/src/main/java/com/example/appsessions/SummaryActivity.kt
+++ app/src/main/java/com/example/appsessions/SummaryActivity.kt
@@ -1,24 +1,24 @@
 package com.example.appsessions
+
 import android.os.Bundle
-import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
-import androidx.appcompat.app.ActionBarDrawerToggle
-import android.content.Intent
+import android.view.MenuItem
 
 
-
-class HomeActivity : AppCompatActivity() {
+class SummaryActivity : AppCompatActivity() {
 
     private lateinit var drawerLayout: DrawerLayout
     private lateinit var navigationView: NavigationView
     private lateinit var toolbar: Toolbar
+    private lateinit var toggle: ActionBarDrawerToggle
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.activity_summary)
 
         drawerLayout = findViewById(R.id.drawer_layout)
         navigationView = findViewById(R.id.navigation_view)
@@ -27,27 +27,25 @@ class HomeActivity : AppCompatActivity() {
         setSupportActionBar(toolbar)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        val toggle = ActionBarDrawerToggle(
+        toggle = ActionBarDrawerToggle(
             this,
             drawerLayout,
             toolbar,
             R.string.navigation_drawer_open,
             R.string.navigation_drawer_close
         )
-
         drawerLayout.addDrawerListener(toggle)
         toggle.syncState()
-
         setupNavigationMenu()
+
     }
+
     fun setupNavigationMenu() {
         navigationView.setNavigationItemSelectedListener { menuItem ->
             // Manejar los clics en los elementos del menú aquí
             when (menuItem.itemId) {
                 R.id.nav_item1 -> {
                     // Acción para el elemento del menú 1
-                    val intent = Intent(this, SummaryActivity::class.java)
-                    startActivity(intent)
                     drawerLayout.closeDrawers()
                     return@setNavigationItemSelectedListener true
                 }
@@ -67,13 +65,14 @@ class HomeActivity : AppCompatActivity() {
         }
     }
 
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return true
+    }
+
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
-        if (item.itemId == android.R.id.home) {
-            if (drawerLayout.isDrawerOpen(navigationView)) {
-                drawerLayout.closeDrawer(navigationView)
-            } else {
-                drawerLayout.openDrawer(navigationView)
-            }
+        if (toggle.onOptionsItemSelected(item)) {
             return true
         }
         return super.onOptionsItemSelected(item)
